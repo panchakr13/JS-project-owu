5c994c0b376eaab79581f0c1172d34c7d0149f9f
panchakr13 <panchakr13@gmail.com>
2024-04-01 10:25:51 +0300

    upd

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ fetch(URL)
     .then(res => res.json())
     .then(users => {
 
-        let usersDiv = document.createElement('div')
+        const usersDiv = document.createElement('div')
         usersDiv.classList.add('users-div')
         document.body.appendChild(usersDiv)
 
 
         for (const user of users) {
-            let userGlobalBlock = document.createElement('div')
+            const userGlobalBlock = document.createElement('div')
             userGlobalBlock.classList.add('user-global-block')
 
 
-            let userBlock = document.createElement('div')
+            const userBlock = document.createElement('div')
             userBlock.classList.add('user-block')
             userBlock.innerHTML = `<span>${user.id}. </span>
                                    <span>${user.name}</span>`
@@ -22,7 +22,7 @@ fetch(URL)
             userGlobalBlock.appendChild(userBlock)
 
 
-            let button = document.createElement('button')
+            const button = document.createElement('button')
             button.innerText = `click me`
 
             button.addEventListener('click', () => {
